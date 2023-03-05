0fd2e441030887516aed903bca108c48acd5a7b5
FerGlez230 <ferglez230@gmail.com>
2023-03-05 17:03:50 -0600

    feat: table mock up

--- src/app/common/angular-material.module.ts
+++ src/app/common/angular-material.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule} from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
   declarations: [],
@@ -14,20 +20,30 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     CommonModule,
     LayoutModule,
     MatButtonModule,
+    MatDividerModule,
+    MatFormFieldModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
+    MatTableModule,
     MatTabsModule,
     MatToolbarModule,
+    MatPaginatorModule,
+    MatInputModule,
   ], 
   exports: [
     LayoutModule,
     MatButtonModule,
+    MatDividerModule,
+    MatFormFieldModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
+    MatTableModule,
     MatTabsModule,
     MatToolbarModule,
+    MatPaginatorModule,
+    MatInputModule,
   ]
 })
 export class AngularMaterialModule { }
